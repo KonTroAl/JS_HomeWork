9156972e1983dad225b56afa41adc621b23ec207
KonTroAl <troshenkin.k@yandex.ru>
2022-05-14 18:37:33 +0500

    1-е задание выполнено
    Сделана первоначальная верстка сайта и добавлены изображения для товаров

--- project/js/main.js
+++ project/js/main.js
@@ -1,18 +1,20 @@
 const products = [
     { id: 1, title: 'Notebook', price: 2000, image: 'notebook' },
-    { id: 2, title: 'Mouse', price: 20, image: 'notebook' },
-    { id: 3, title: 'Keyboard', price: 200, image: 'notebook' },
-    { id: 4, title: 'Gamepad', price: 50, image: 'notebook' },
+    { id: 2, title: 'Mouse', price: 20, image: 'mouse' },
+    { id: 3, title: 'Keyboard', price: 200, image: 'keyboard' },
+    { id: 4, title: 'Gamepad', price: 50, image: 'gamepad' },
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
 const renderProduct = (item) => {
 
-    return `<div class="product-item">
-                <h3>${item.title}</h3>
-                <img src="images/${item.image}.jpg" alt="${item.image}">
-                <p>${item.price}</p>
-                <button class="btn btn-outline-success">Купить</button>
+    return `<div class="product-item card">
+                <img class="product-image card-img-top" src="images/${item.image}.jpg" alt="${item.image}">
+                <div class="card-body">
+                    <h5 class="card-title">${item.title}</h5>
+                    <p class="card-text" >${item.price}</p>
+                    <button class="product-button btn btn-outline-success">Buy</button>
+                </div>
             </div>`
 };
 const renderPage = list => {
