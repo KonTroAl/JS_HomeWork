b94d691a186ba6dc66ad30e704fd9d5dc525d8be
KonTroAl <troshenkin.k@yandex.ru>
2022-05-16 20:37:16 +0500

    Старт работы над ДЗ
    Переработка кода под ООП

--- project/js/main.js
+++ project/js/main.js
@@ -1,27 +1,47 @@
-const products = [
-    { id: 1, title: 'Notebook', price: 2000, image: 'notebook' },
-    { id: 2, title: 'Mouse', price: 20, image: 'mouse' },
-    { id: 3, title: 'Keyboard', price: 200, image: 'keyboard' },
-    { id: 4, title: 'Gamepad', price: 50, image: 'gamepad' },
-];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
-const renderProduct = (item) => {
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render();
+    }
 
-    return `<div class="product-item card">
-                <img class="product-image card-img-top" src="images/${item.image}.jpg" alt="${item.image}">
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000, image: 'notebook' },
+            { id: 2, title: 'Mouse', price: 20, image: 'mouse' },
+            { id: 3, title: 'Keyboard', price: 200, image: 'keyboard' },
+            { id: 4, title: 'Gamepad', price: 50, image: 'gamepad' },
+        ];
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product) {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.image = product.image;
+    }
+
+    render() {
+        return `<div class="product-item card">
+                <img class="product-image card-img-top" src="images/${this.image}.jpg" alt="${this.image}">
                 <div class="card-body">
-                    <h5 class="card-title">${item.title}</h5>
-                    <p class="card-text" >${item.price}</p>
+                    <h5 class="card-title">${this.title}</h5>
+                    <p class="card-text" >${this.price}</p>
                     <button class="product-button btn btn-outline-success">Buy</button>
                 </div>
             </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
-    let format_productsList = productsList.join('');
-    console.log(format_productsList);
-    document.querySelector('.products').innerHTML = format_productsList;
-};
+    }
+}
 
-renderPage(products);
+let list = new ProductList()
