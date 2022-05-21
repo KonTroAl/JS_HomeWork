7457b20a6e4508e82c2b0f6e66a5d80ac7ac835d
KonTroAl <troshenkin.k@yandex.ru>
2022-05-21 11:53:13 +0500

    Подготовка скриптов к ДЗ

--- HomeWork/js/main.js
+++ HomeWork/js/main.js
@@ -1,23 +1,60 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50}
-];
-//Функция для формирования верстки каждого товара
-const renderProduct = (product,img='https://via.placeholder.com/200x150') => {
-    return `<div class="product-item">
-                <img src="${img}">
-                <h3>${product.title}</h3>
-                <p>${product.price}</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
-const renderPage = list => {
-    document.querySelector('.products').innerHTML =
-        list.map(product => renderProduct(product));
-};
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._getProducts()
+            .then(data => {
+                this.goods = data;
+                this.render()
+            });
+        this.sumGoodsPrice();
+    }
+
+    _getProducts() {
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+
+    sumGoodsPrice() {
+        let result = 0
+        for (let product of this.goods) {
+            result += parseInt(product.price)
+        }
+        console.log(result);
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.title = product.product_name;
+        this.id = product.id_product;
+        this.price = product.price;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="product-item">
+        <img src="${this.img}">
+        <h3>${this.title}</h3>
+        <p>${this.price}</p>
+        <button class="buy-btn">Купить</button>
+    </div>`
+    };
+}
 
 
-
-renderPage(products);
+let list = new ProductList();
+console.log(list);
